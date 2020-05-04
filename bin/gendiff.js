e5d6a455986c2dc802ec1a4890b07a3cab7d4265
elvolt <sidelevCS@gmail.com>
2020-05-04 19:39:06 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ import program from 'commander';
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
 
 console.log(program.args);
