ec81e3ec44379ce79780870868e893530667ff02
elvolt <sidelevCS@gmail.com>
2020-05-31 21:15:50 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
+import 'lodash.product';
+import _ from 'lodash';
 import genDiff from '../src/index.js';
 
 const getFixtureAbsolutePath = (fileName) => path.join(path.resolve(), '__fixtures__', fileName);
@@ -13,39 +15,20 @@ const getPaths = (fileName1, fileName2) => [
 const resultStylish = fs.readFileSync(`${getFixtureAbsolutePath('resultStylish.txt')}`, 'utf-8');
 const resultPlain = fs.readFileSync(`${getFixtureAbsolutePath('resultPlain.txt')}`, 'utf-8');
 const resultJSON = JSON.stringify(JSON.parse(fs.readFileSync(`${getFixtureAbsolutePath('resultJSON.json')}`)));
-const [filePath1JSON, filePath2JSON] = [...getPaths('before.json', 'after.json')];
-const [filePath1YML, filePath2YML] = [...getPaths('before.yml', 'after.yml')];
-const [filePath1INI, filePath2INI] = [...getPaths('before.ini', 'after.ini')];
 
-test.each([
-  [filePath1JSON, filePath2JSON, resultStylish],
-  [filePath1YML, filePath2YML, resultStylish],
-  [filePath1INI, filePath2INI, resultStylish],
-])('test genDiff with stylish output',
-  (filePath1, filePath2, expected) => {
-    expect(genDiff(filePath1, filePath2, 'stylish')).toBe(expected);
-  });
+const fileTypes = ['yml', 'ini', 'json'];
+const resultsFormats = [['stylish', resultStylish], ['plain', resultPlain], ['json', resultJSON]];
+const inputFilesPaths = fileTypes.map((type) => getPaths(`before.${type}`, `after.${type}`));
 
-test.each([
-  [filePath1JSON, filePath2JSON, resultPlain],
-  [filePath1YML, filePath2YML, resultPlain],
-  [filePath1INI, filePath2INI, resultPlain],
-])('test genDiff with plain output',
-  (filePath1, filePath2, expected) => {
-    expect(genDiff(filePath1, filePath2, 'plain')).toBe(expected);
-  });
-
-test.each([
-  [filePath1JSON, filePath2JSON, resultJSON],
-  [filePath1YML, filePath2YML, resultJSON],
-  [filePath1INI, filePath2INI, resultJSON],
-])('test genDiff with json output',
-  (filePath1, filePath2, expected) => {
-    expect(genDiff(filePath1, filePath2, 'json')).toBe(expected);
+test.each(
+  _.product(inputFilesPaths, resultsFormats),
+)('test genDiff',
+  ([filePath1, filePath2], [formatter, expected]) => {
+    expect(genDiff(filePath1, filePath2, formatter)).toBe(expected);
   });
 
 test('test incorrect output format', () => {
   expect(() => {
-    genDiff(filePath1JSON, filePath2JSON, 'html');
+    genDiff(...getPaths('before.json', 'after.json'), 'html');
   }).toThrow('Error: format html is not supported');
 });
