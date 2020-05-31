70bf7342b5f52fa7c8cbb3a7df43fbf763bfee74
elvolt <sidelevCS@gmail.com>
2020-05-31 22:24:53 +0300

    refactor parsers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+import path from 'path';
+import process from 'process';
+import fs from 'fs';
 import parse from './parsers.js';
 import getDiff from './getdiff.js';
 import render from './formatters/index.js';
 
+const getAbsoluteFilePath = (filePath) => (filePath.startsWith('/')
+  ? filePath : path.resolve(process.cwd(), filePath));
+
 export default (filePath1, filePath2, format) => {
-  const file1Parsed = parse(filePath1);
-  const file2Parsed = parse(filePath2);
+  const file1AbsolutePath = getAbsoluteFilePath(filePath1);
+  const file2AbsolutePath = getAbsoluteFilePath(filePath2);
+
+  const file1Data = fs.readFileSync(file1AbsolutePath, 'utf8');
+  const file2Data = fs.readFileSync(file2AbsolutePath, 'utf8');
+
+  const file1Extension = path.extname(file1AbsolutePath);
+  const file2Extension = path.extname(file2AbsolutePath);
+
+  const file1Parsed = parse(file1Extension)(file1Data);
+  const file2Parsed = parse(file2Extension)(file2Data);
 
   const differences = getDiff(file1Parsed, file2Parsed);
 
--- src/parsers.js
+++ src/parsers.js
@@ -1,27 +1,18 @@
-import fs from 'fs';
-import path from 'path';
-import process from 'process';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const getAbsolutePath = (relativePath) => path.resolve(process.cwd(), relativePath);
-
-export default (filePath) => {
-  const absoluteFilepath = filePath.startsWith('/') ? filePath : getAbsolutePath(filePath);
-  const format = path.extname(filePath);
-  const data = fs.readFileSync(absoluteFilepath, 'utf8');
-
-  if (format === '.yml' || format === '.yaml') {
+export default (extension) => (data) => {
+  if (extension === '.yml' || extension === '.yaml') {
     return yaml.safeLoad(data);
   }
 
-  if (format === '.ini') {
+  if (extension === '.ini') {
     return ini.parse(data);
   }
 
-  if (format === '.json') {
+  if (extension === '.json') {
     return JSON.parse(data);
   }
 
-  throw new Error(`Error: format ${format} is not supported`);
+  throw new Error(`Error: format ${extension} is not supported`);
 };
