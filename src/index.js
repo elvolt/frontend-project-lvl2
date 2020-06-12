eaa384e89688a5df87754ba61e96cdaaff380234
elvolt <sidelevCS@gmail.com>
2020-06-12 13:47:29 +0300

    refactor. Add parseFile()

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import path from 'path';
-import process from 'process';
 import fs from 'fs';
 import parse from './parsers.js';
 import getDiff from './getdiff.js';
 import render from './formatters/index.js';
 
+const parseFile = (filePath) => {
+  const data = fs.readFileSync(filePath, 'utf8');
+  const extension = path.extname(filePath).slice(1);
+
+  return parse(extension)(data);
+};
+
 export default (filePath1, filePath2, format) => {
-  const file1AbsolutePath = path.resolve(process.cwd(), filePath1);
-  const file2AbsolutePath = path.resolve(process.cwd(), filePath2);
+  const data1 = parseFile(filePath1);
+  const data2 = parseFile(filePath2);
 
-  const file1Data = fs.readFileSync(file1AbsolutePath, 'utf8');
-  const file2Data = fs.readFileSync(file2AbsolutePath, 'utf8');
-
-  const file1Extension = path.extname(file1AbsolutePath).slice(1);
-  const file2Extension = path.extname(file2AbsolutePath).slice(1);
-
-  const file1Parsed = parse(file1Extension)(file1Data);
-  const file2Parsed = parse(file2Extension)(file2Data);
-
-  const differences = getDiff(file1Parsed, file2Parsed);
+  const differences = getDiff(data1, data2);
 
   return render(format)(differences);
 };
